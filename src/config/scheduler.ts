6f17789ac272e54b09455783a9b16f1873508df6
vinkakniv <vinka.aeris@gmail.com>
2025-07-20 23:31:32 +0700

    fix weekly challenge schaduler

--- src/config/scheduler.ts
+++ src/config/scheduler.ts
@@ -3,9 +3,9 @@ import { generateWeeklyChallenge } from '../services/challenge/createChallenge.s
 
 
 // Cron: Senin pukul 09:00 WIB (WIB = UTC+7 → 02:00 UTC)
-//cron.schedule('0 2 * * 1', async () => {
+cron.schedule('0 2 * * 1', async () => {
 //cron.schedule('38 14 * * 0', async () => {
-cron.schedule('* * * * *', async () => {
+//cron.schedule('* * * * *', async () => {
   console.log('⏰ Menjalankan scheduler weekly challenge...')
   const result = await generateWeeklyChallenge()
   console.log('⏰ dia berhasil...')
