fcee3be029603023b38a07ee6142380521e7d628
evelynphs <evelyn.p.h.silalahi@gmail.com>
2025-07-20 19:26:52 +0700

    chore: delete chatbot and challenge directory

--- src/challenge/generateChallenge.ts
+++
@@ -1,4 +0,0 @@
-export const getChallengeContentFromGemini = async (): Promise<string> => {
-  // Replace ini kalau kamu sudah pakai Gemini beneran
-  return '🌱 Challenge minggu ini: Lakukan 1 kebaikan kecil kepada orang asing.'
-}
