e8c10640f54453c7bb748d15d6c58edd32780704
evelynphs <evelyn.p.h.silalahi@gmail.com>
2025-07-23 18:13:16 +0700

    feat: deactivate previous week's challenge in scheduler

--- src/repositories/challenge/challenge.repositories.ts
+++ src/repositories/challenge/challenge.repositories.ts
@@ -6,6 +6,17 @@ export const findChallengeByTimestamp = async (timestamp: Date) => {
   })
 }
 
+export const findChallengeByTimeRange = async (gte: Date, lte: Date) => {
+  return prisma.challenge.findFirst({
+    where: {
+      timestamp: {
+        gte: gte,
+        lte: lte,
+      },
+    },
+  })
+}
+
 export const findChallengeById = async (id: string) => {
   return prisma.challenge.findUnique({
     where: { id },
@@ -22,7 +33,7 @@ export const createChallenge = async (content: string, timestamp: Date) => {
 }
 
 
-export const inactivateChallenge = async (id: string) => {
+export const deactivateChallengeById = async (id: string) => {
   return prisma.challenge.update({
     where: { id },
     data: { isActive: false },
